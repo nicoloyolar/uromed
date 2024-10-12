87ebea6e13fab27032df91bcda6750dcc6077ffc
Nicolás <nicolas.iloyolar@gmail.com>
2024-10-11 23:25:35 -0300

    estilos y autenticacion en login

--- uromed/usuarios/views.py
+++ uromed/usuarios/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')  
+        else:
+            messages.error(request, 'Nombre de usuario o contraseña incorrectos.')
+    
     return render(request, 'usuarios/login.html')
 
 def home_view(request):
