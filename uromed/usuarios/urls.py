f439b177bec3c37d6f9a555cd92d756fcd416158
Nicolás <nicolas.iloyolar@gmail.com>
2024-12-12 02:40:40 -0300

    integra detalle del paciente

--- uromed/usuarios/urls.py
+++ uromed/usuarios/urls.py
@@ -5,15 +5,9 @@ urlpatterns = [
     path('', login_view, name='login'),
     path('home/', home_view, name='home'),
     path('logout/', logout_view, name='logout'),
-    path('agenda/', agenda_view, name='agenda_view'),  
-    path('detalle_paciente/', detalle_paciente_view, name='detalle_paciente'),
-    path('informes/', informes_view, name='informes_view'),  
-    path('nuevo_paciente/', nuevo_paciente_view, name='nuevo_paciente'), 
-    path('examenes/', examenes_view, name='examenes_view'), 
-    path('lista_examenes/', lista_examenes_view, name='lista_examenes'),
-    path('lista_pacientes/', lista_pacientes_view, name='lista_pacientes'),
+    path('detalle_paciente/<int:paciente_id>/', detalle_paciente_view, name='detalle_paciente'),
+    path('eliminar_paciente/<int:id>/', eliminar_paciente_view, name='eliminar_paciente'),
+    path('nuevo_paciente/', nuevo_paciente_view, name='nuevo_paciente'),
     path('paciente/<int:paciente_id>/', nuevo_o_actualizar_paciente, name='nuevo_o_actualizar_paciente'),
     path('paciente/', nuevo_o_actualizar_paciente, name='nuevo_o_actualizar_paciente'),
-    path('eliminar_paciente/<int:id>/', eliminar_paciente_view, name='eliminar_paciente'),
-
 ]
--- uromed/usuarios/views.py
+++ uromed/usuarios/views.py
@@ -28,36 +28,13 @@ def logout_view(request):
     logout(request)  
     return redirect('login')
 
+@login_required
 def home_view(request):
-    return render(request, 'usuarios/home.html')
-
-def agenda_view(request):
-    return render(request, 'usuarios/agenda.html')  
-
-def informes_view(request):
-    return render(request, 'usuarios/informes.html') 
+    pacientes = Paciente.objects.all()  
+    return render(request, 'usuarios/home.html', {'pacientes': pacientes})
 
 def nuevo_paciente_view(request):
     return render(request, 'usuarios/nuevo_paciente.html')  
-
-@login_required
-def examenes_view(request):
-    examenes = Examen.objects.all()
-    return render(request, 'usuarios/lista_examenes.html', {'examenes': examenes})
-
-@login_required
-def lista_examenes_view(request):
-    examenes = Examen.objects.all()
-    return render(request, 'usuarios/lista_examenes.html', {'examenes': examenes})
-
-@login_required
-def lista_pacientes_view(request):
-    pacientes = Paciente.objects.all()  
-    return render(request, 'usuarios/lista_pacientes.html', {'pacientes': pacientes})
-
-@login_required
-def detalle_paciente_view(request):
-    return render(request, 'usuarios/detalle_paciente.html')
 
 def nuevo_o_actualizar_paciente(request, paciente_id=None):
     
@@ -66,74 +43,78 @@ def nuevo_o_actualizar_paciente(request, paciente_id=None):
         paciente = get_object_or_404(Paciente, pk=paciente_id)
 
     if request.method == 'POST':
-        nombre = request.POST.get('nombre')
-        apellido = request.POST.get('apellido')
-        edad = request.POST.get('edad')
-        genero = request.POST.get('genero')
-        direccion = request.POST.get('direccion')
-        telefono = request.POST.get('telefono')
-        email = request.POST.get('email')
-        fecha_nacimiento = request.POST.get('fecha_nacimiento')
-        estado_civil = request.POST.get('estado_civil')
-        antecedentes_medicos = request.POST.get('antecedentes_medicos')
-        alergias = request.POST.get('alergias')
-        medicamentos = request.POST.get('medicamentos')
-        presion_arterial = request.POST.get('presion_arterial')
-        frecuencia_cardiaca = request.POST.get('frecuencia_cardiaca')
-        peso = request.POST.get('peso')
-        talla = request.POST.get('talla')
-        saturacion = request.POST.get('saturacion')
-        temperatura = request.POST.get('temperatura')
-        frecuencia_respiratoria = request.POST.get('frecuencia_respiratoria')
+        nombre                                  = request.POST.get('nombre')
+        apellido                                = request.POST.get('apellido')
+        edad                                    = request.POST.get('edad')
+        genero                                  = request.POST.get('genero')
+        direccion                               = request.POST.get('direccion')
+        telefono                                = request.POST.get('telefono')
+        email                                   = request.POST.get('email')
+        fecha_nacimiento                        = request.POST.get('fecha_nacimiento')
+        estado_civil                            = request.POST.get('estado_civil')
+        antecedentes_medicos                    = request.POST.get('antecedentes_medicos')
+        alergias                                = request.POST.get('alergias')
+        medicamentos                            = request.POST.get('medicamentos')
+        presion_arterial                        = request.POST.get('presion_arterial')
+        frecuencia_cardiaca                     = request.POST.get('frecuencia_cardiaca')
+        peso                                    = request.POST.get('peso')
+        talla                                   = request.POST.get('talla')
+        saturacion                              = request.POST.get('saturacion')
+        temperatura                             = request.POST.get('temperatura')
+        frecuencia_respiratoria                 = request.POST.get('frecuencia_respiratoria')
 
         if paciente:
-            paciente.nombre = nombre
-            paciente.apellido = apellido
-            paciente.edad = edad
-            paciente.genero = genero
-            paciente.direccion = direccion
-            paciente.telefono = telefono
-            paciente.email = email
-            paciente.fecha_nacimiento = fecha_nacimiento
-            paciente.estado_civil = estado_civil
-            paciente.antecedentes_medicos = antecedentes_medicos
-            paciente.alergias = alergias
-            paciente.medicamentos = medicamentos
-            paciente.presion_arterial = presion_arterial
-            paciente.frecuencia_cardiaca = frecuencia_cardiaca
-            paciente.peso = peso
-            paciente.talla = talla
-            paciente.saturacion = saturacion
-            paciente.temperatura = temperatura
-            paciente.frecuencia_respiratoria = frecuencia_respiratoria
+            paciente.nombre                     = nombre
+            paciente.apellido                   = apellido
+            paciente.edad                       = edad
+            paciente.genero                     = genero
+            paciente.direccion                  = direccion
+            paciente.telefono                   = telefono
+            paciente.email                      = email
+            paciente.fecha_nacimiento           = fecha_nacimiento
+            paciente.estado_civil               = estado_civil
+            paciente.antecedentes_medicos       = antecedentes_medicos
+            paciente.alergias                   = alergias
+            paciente.medicamentos               = medicamentos
+            paciente.presion_arterial           = presion_arterial
+            paciente.frecuencia_cardiaca        = frecuencia_cardiaca
+            paciente.peso                       = peso
+            paciente.talla                      = talla
+            paciente.saturacion                 = saturacion
+            paciente.temperatura                = temperatura
+            paciente.frecuencia_respiratoria    = frecuencia_respiratoria
             paciente.save()
         else:
             paciente = Paciente(
-                nombre=nombre,
-                apellido=apellido,
-                edad=edad,
-                genero=genero,
-                direccion=direccion,
-                telefono=telefono,
-                email=email,
-                fecha_nacimiento=fecha_nacimiento,
-                estado_civil=estado_civil,
-                antecedentes_medicos=antecedentes_medicos,
-                alergias=alergias,
-                medicamentos=medicamentos,
-                presion_arterial=presion_arterial,
-                frecuencia_cardiaca=frecuencia_cardiaca,
-                peso=peso,
-                talla=talla,
-                saturacion=saturacion,
-                temperatura=temperatura,
-                frecuencia_respiratoria=frecuencia_respiratoria
+                nombre                          = nombre,
+                apellido                        = apellido,
+                edad                            = edad,
+                genero                          = genero,
+                direccion                       = direccion,
+                telefono                        = telefono,
+                email                           = email,
+                fecha_nacimiento                = fecha_nacimiento,
+                estado_civil                    = estado_civil,
+                antecedentes_medicos            = antecedentes_medicos,
+                alergias                        = alergias,
+                medicamentos                    = medicamentos,
+                presion_arterial                = presion_arterial,
+                frecuencia_cardiaca             = frecuencia_cardiaca,
+                peso                            = peso,
+                talla                           = talla,
+                saturacion                      = saturacion,
+                temperatura                     = temperatura,
+                frecuencia_respiratoria         = frecuencia_respiratoria
             )
             paciente.save()
 
         return redirect('lista_pacientes')
 
     return render(request, 'nuevo_paciente.html', {'paciente': paciente})
+
+def detalle_paciente_view(request, paciente_id):
+    paciente = get_object_or_404(Paciente, id=paciente_id)
+    return render(request, 'usuarios/detalle_paciente.html', {'paciente': paciente})
 
 def eliminar_paciente_view(request, id):
     paciente = get_object_or_404(Paciente, id=id)
